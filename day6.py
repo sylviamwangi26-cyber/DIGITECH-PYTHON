e4a967299c64cf58ce43f75da6e45eb71f708762
sylviamwangi26_cyber <sylviamwangi26@gmail.com>
2025-10-09 12:10:23 +0300

    updates

---
+++ day6.py
@@ -0,0 +1,21 @@
+a = 5
+b = 4
+
+calc = a & b
+binary=bin(calc)
+
+print(f"{calc} and it's binary is {binary}")
+
+a =10
+b = 8
+calc = a ^ b
+binary=bin(calc)
+
+print(f"{calc} and it's binary is {binary}")
+
+a = 10
+b = 8
+calc = a | b
+binary=bin(calc)
+
+print(f"{calc} and it's binary is {binary}")
