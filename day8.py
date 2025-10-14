a178f741d273c4d448011286951465bf3d5e7f04
sylviamwangi26_cyber <sylviamwangi26@gmail.com>
2025-10-14 11:30:08 +0300

    upadates

---
+++ day8.py
@@ -0,0 +1,27 @@
+#define a function
+# def get_pass(name, age):
+#     if age>= 17 :
+#         return f"{name},you are not allowed to go in!!"
+#     else:
+#         return f"{name},you are allowed to go in!!"
+    
+# #regenerate the name and age
+# user_name = input("Enter your name: ")
+# user_age = int(input("Enter your age: "))
+
+# final = get_pass(user_name, user_age)
+# print(final)
+
+
+
+
+
+def get_access():
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+
+    if age<=17:
+        return f"{name},you are allowed to go in!!"
+    else:
+        print(f"{name},you are not allowed to go in!!")
+get_access()
